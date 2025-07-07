c7e75823dd00f42a001f6af40a175783ca4f2057
ark <ark@users.noreply.github.com>
2025-07-07 09:26:28 +0700

    tugas: 4 change title

--- generate_pdf.py
+++ generate_pdf.py
@@ -3,7 +3,7 @@ from weasyprint import HTML
 
 TEMPLATE_FILE = 'report_template.html'
 SCREENSHOT_DIR = 'screenshots'
-OUTPUT_PDF = 'TUGAS 4 - 2311510438 - Aris Kurniawan.pdf'
+OUTPUT_PDF = 'TUGAS 3-a - 2311510438 - Aris Kurniawan.pdf'
 
 # Ambil daftar file PNG dari folder screenshots
 images = [f for f in os.listdir(SCREENSHOT_DIR) if f.endswith('.png')]
